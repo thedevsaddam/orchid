e59c1b982bd994148bf589ace5db099ef7803acd
Saddam H <thedevsaddam@gmail.com>
2017-04-09 15:43:03 +0600

    Working  on session  middleware

--- vendor/app/config/session.go
+++ vendor/app/config/session.go
@@ -8,22 +8,24 @@ import (
 )
 
 func InitializeSession() gin.HandlerFunc {
-	sessionDriver := os.Getenv("SESSION_STORE_DRIVER")
-	sessionName := os.Getenv("SESSION_STORE_NAME")
-	appKey := os.Getenv("APP_KEY")
-	switch sessionDriver {
-	case "cookie":
-		store := sessions.NewCookieStore([]byte(appKey))
-		return sessions.Sessions(sessionName, store)
-	case "redis":
-		store, _ := sessions.NewRedisStore(10, "tcp", os.Getenv("SESSION_HOST")+":"+os.Getenv("SESSION_PORT"), "", []byte(appKey))
-		return sessions.Sessions(sessionName, store)
-	case "memcache":
-		store := sessions.NewMemcacheStore(memcache.New(os.Getenv("SESSION_HOST")+":"+os.Getenv("SESSION_PORT")), "", []byte(appKey))
-		return sessions.Sessions(sessionName, store)
-	default:
-		store := sessions.NewCookieStore([]byte(appKey))
-		return sessions.Sessions(sessionName, store)
+	return func(c *gin.Context) {
+		sessionDriver := os.Getenv("SESSION_STORE_DRIVER")
+		sessionName := os.Getenv("SESSION_STORE_NAME")
+		appKey := os.Getenv("APP_KEY")
+		switch sessionDriver {
+		case "cookie":
+			store := sessions.NewCookieStore([]byte(appKey))
+			sessions.Sessions(sessionName, store)
+		case "redis":
+			store, _ := sessions.NewRedisStore(10, "tcp", os.Getenv("SESSION_HOST") + ":" + os.Getenv("SESSION_PORT"), "", []byte(appKey))
+			sessions.Sessions(sessionName, store)
+		case "memcache":
+			store := sessions.NewMemcacheStore(memcache.New(os.Getenv("SESSION_HOST") + ":" + os.Getenv("SESSION_PORT")), "", []byte(appKey))
+			sessions.Sessions(sessionName, store)
+		default:
+			store := sessions.NewCookieStore([]byte(appKey))
+			sessions.Sessions(sessionName, store)
+		}
+		c.Next()
 	}
-
 }
